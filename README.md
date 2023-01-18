96a8c1bcb0cea333b323587a51f55f73c72ac35a
Joseph Daws <daws.joseph@gmail.com>
2023-01-17 23:28:50 -0500

    added training log and journal

--- README.md
+++ README.md
@@ -7,7 +7,15 @@ who says life can't be like an RPG
 # Goals
 
 - Lose 100 pounds from 272 --> 172 in the calendar year 2023
+- Perform 3 activities per week with 30 minutes or more spent in zone 3 heart rate
 
 # Progress
 
+## Weight
+
 ![Weight over time](./images/weight.png)
+
+
+## Activities
+
+![Activities this year](./images/training.png)
--- stats/visualize.py
+++ stats/visualize.py
@@ -4,6 +4,7 @@ Create visualizations of the personal statistics in this project
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import calmap
 
 from stats.constants import DATA_DIR, IMAGE_DIR, POUNDS_LOST_PER_WEEK
 
@@ -36,5 +37,31 @@ def create_weight_plot():
     plt.savefig(os.path.abspath(os.path.join(IMAGE_DIR, "weight.png")))
 
 
+def create_training_days_plot():
+    activity_data = pd.read_csv(
+        os.path.abspath(os.path.join(DATA_DIR, "training/heart_rate.csv"))
+    )
+    activity_data["recorded_at"] = pd.to_datetime(activity_data["recorded_at"])
+    dates = activity_data["recorded_at"]
+    activities = activity_data["z3"].apply(lambda x: int(x[3:5]))
+    activities.index = dates
+    fig, ax = plt.subplots()
+
+    calmap.calendarplot(
+        activities,
+        monthticks=3,
+        daylabels="MTWTFSS",
+        dayticks=[0, 2, 4, 6],
+        cmap="YlGn",
+        fillcolor="grey",
+        linewidth=0,
+    )
+
+    plt.title("Heart Rate Z3")
+    plt.plot()
+    plt.savefig(os.path.abspath(os.path.join(IMAGE_DIR, "training.png")))
+
+
 if __name__ == "__main__":
     create_weight_plot()
+    create_training_days_plot()
